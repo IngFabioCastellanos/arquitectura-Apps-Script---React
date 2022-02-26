27c0e49975cfb31ffef20cb4df37d080fc706ed5
fabio castellanos <fcastellanos782@gmail.com>
2022-02-26 13:26:09 -0500

    configuraciones en backend

---
+++ backend/config/env.js
@@ -0,0 +1,11 @@
+/**
+ * env_
+ *
+ * @return {Object} objeto con los parametros globales
+ */
+function env_() {
+  return {
+    ID_DATABASE: "id base de datos",
+    SH_REGISTRO_USUARIOS: "registro usuarios",
+  };
+}
