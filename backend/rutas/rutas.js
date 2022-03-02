9c724d1deec20a27d1cf0f4daa7fa611ebda23fd
fabio castellanos <fcastellanos782@gmail.com>
2022-03-02 18:49:14 -0500

    manejo de rutas desde el backend

---
+++ backend/rutas/rutas.js
@@ -0,0 +1,21 @@
+const config = ["frontend/config/constantesCdn"];
+
+const componentes = [];
+
+const helpers = [];
+
+const redux = [];
+
+const rutas = ["frontend/routes/Routes"];
+
+const vistas = [];
+
+const importaciones = [
+  ...config,
+  ...componentes,
+  ...helpers,
+  ...redux,
+  ...rutas,
+  ...vistas,
+  "frontend/App",
+];
